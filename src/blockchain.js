8e8e12fe5edb226565ca8b37dff23a596edf4de5
Huy Chau <chaungochuy0709.gmail.com>
2022-02-11 23:15:42 +0700

    signature for transaction

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
-
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction{
     constructor(fromAddress, toAddress, amount){
@@ -7,7 +8,30 @@ class Transaction{
         this.toAddress = toAddress;
         this.amount = amount;
     }
-    
+    calculateHash(){
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+    signTransaction(signingKey){
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign transactions for other wallets')
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = sig.toDER('hex')
+    }
+    isValid(){
+        if (this.fromAddress == null){
+            return true;
+        }
+
+        if (!this.signature || this.signature.length == 0){
+            throw new Error('No signature in this transaction');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature)
+    }
 }
 
 class Block{
@@ -28,6 +52,14 @@ class Block{
         }
         console.log("Block Mined: " + this.hash)
     }
+    hasValidTransaction(){
+        for (const tx of this.transactions){
+            if (!tx.isValid()){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 class BlockChain{
@@ -41,19 +73,29 @@ class BlockChain{
     createFirstBlock(){
         return new Block('11/2/2022', 'FirstBlock', '0')
     }
-    getLastestBlock(){
+    getLatestBlock(){
         return this.chain[this.chain.length - 1]
     }
     minePendingTransactions(miningRewardAddress){
-        let block = new Block(Date.now(), this.pendingTransactions);
+        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
+        const block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
         block.mineBlock(this.difficulty);
 
-        console.log("Block successfully mined");
+        console.log('Block successfully mined!');
         this.chain.push(block);
 
-        this.pendingTransactions = [new Transaction(null, miningRewardAddress, this.miningReward)]
+        this.pendingTransactions = [];
     }
-    createTransaction(transaction){
+    addTransaction(transaction){
+        if (!transaction.fromAddress || !transaction.toAddress){
+            throw new Error("Transaction must has from and to address")
+        }
+
+        if (!transaction.isValid()){
+            throw new Error("cannot add invalid transaction")
+        }
         this.pendingTransactions.push(transaction)
     }
     getBalanceOfAddress(address){
@@ -75,6 +117,10 @@ class BlockChain{
         for (let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i-1];
+
+            if (!currentBlock.hasValidTransaction()){
+                return false;
+            }
             if (currentBlock.hash != currentBlock.calculateHash()){
                 return false;
             }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 const {BlockChain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-let Huycoin = new BlockChain();
+const myKey = ec.keyFromPrivate('b9b64ffaa746660fbcbf763d03b2e4c63af305e5937b3f0924b40ccb99128354');
+const myWallletAddress = myKey.getPublic('hex')
+
+const tx1 = new Transaction(myWallletAddress, 'some public key', 10);
+tx1.signTransaction(myKey);
+
+let HuyCoin = new BlockChain();
+HuyCoin.addTransaction(tx1);
+
+console.log("start mining...");
+HuyCoin.minePendingTransactions(myWallletAddress)
+
+console.log("balance of myWallletAddress", HuyCoin.getBalanceOfAddress(myWallletAddress))
 
 
-Huycoin.createTransaction(new Transaction('address1', 'address2', 100))
-Huycoin.createTransaction(new Transaction('address2', 'address1', 200))
+HuyCoin.chain[1].transactions[0].amount = 100
 
-console.log("start mining...")
-Huycoin.minePendingTransactions('miner')
-
-Huycoin.minePendingTransactions('miner')
-
-console.log('balance of miner: ' + Huycoin.getBalanceOfAddress('miner'))
-
+console.log("is chain valid?", HuyCoin.isChainValid())
